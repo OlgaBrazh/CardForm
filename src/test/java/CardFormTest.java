e524214cca2f6250116af7b8d4f7e6be2ead7574
Olga Brazhkina <emelyanova84@yandex.ru>
2023-02-20 11:44:55 +0500

    Add tests

--- src/test/java/CardFormTest.java
+++ src/test/java/CardFormTest.java
@@ -19,13 +19,148 @@ public class CardFormTest {
         $("span[data-test-id=city] input").setValue ("Пермь");
         $("span[data-test-id=date] input").doubleClick();
         $("span[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("span[data-test-id=date] input").setValue("21.02.2023");
+        $("span[data-test-id=date] input").setValue("24.02.2023");
         $("span[data-test-id=name] input").setValue("Ольга Бражкина");
         $("span[data-test-id=phone] input").setValue("+71234567891");
         $("[data-test-id=agreement]").click();
         $x("//span[text()='Забронировать']").click();
         $x ("//div[contains(text(), 'Успешно!')]").shouldBe(visible, Duration.ofSeconds(15));
         $x ("//div[contains(text(), 'Встреча успешно забронирована на ')]").shouldBe(visible, Duration.ofSeconds(15));
+
+    }
+
+    @Test
+    void shouldWithoutCity() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Березники");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue("24.02.2023");
+        $("[data-test-id=name] input").setValue("Ольга Бражкина");
+        $("[data-test-id=phone] input").setValue("+71234567891");
+        $("[data-test-id=agreement]").click();
+        $x("//span[text()='Забронировать']").click();
+        $x("//span[contains (text (), 'Доставка в выбранный город недоступна')]").shouldBe(appear);
+
+    }
+
+    @Test
+    void shouldTestWithoutDate() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $("span[data-test-id=city] input").setValue("Пермь");
+        $("span[data-test-id=date] input").doubleClick();
+        $("span[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("span[data-test-id=name] input").setValue("Ольга Бражкина");
+        $("span[data-test-id=phone] input").setValue("+71234567891");
+        $("[data-test-id=agreement]").click();
+        $x("//span[text()='Забронировать']").click();
+        $x("//span[text()= 'Неверно введена дата']").shouldBe(visible);
+    }
+
+    @Test
+    void shouldTestNotValidDate() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Пермь");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue("20.02.2023");
+        $("[data-test-id=name] input").setValue("Ольга Бражкина");
+        $("[data-test-id=phone] input").setValue("+71234567891");
+        $("[data-test-id=agreement]").click();
+        $x("//span[text()='Забронировать']").click();
+        $x("//span[contains (text (), 'Заказ на выбранную дату')]").shouldBe(appear);
+
+    }
+
+
+    @Test
+    void shouldLatinName() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Пермь");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").sendKeys("24.02.2023");
+        $("[data-test-id=name] input").setValue("Olga Brazhkina");
+        $("[data-test-id=phone] input").setValue("+79181102009");
+        $("[class=checkbox__box]").click();
+        $("[class=button__text]").click();
+        $x("//span[contains (text (), 'Имя и Фамилия')]").shouldBe(appear);
+    }
+
+    @Test
+    void shouldNameWithDash() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Пермь");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").sendKeys("24.02.2023");
+        $("[data-test-id=name] input").setValue("Ольга-Бражкина");
+        $("[data-test-id=phone] input").setValue("+79181102009");
+        $("[class=checkbox__box]").click();
+        $("[class=button__text]").click();
+        $x ("//div[contains(text(), 'Успешно!')]").shouldBe(visible, Duration.ofSeconds(15));
+        $x ("//div[contains(text(), 'Встреча успешно забронирована на ')]").shouldBe(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldWithoutName() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Пермь");
+        $("span[data-test-id=date] input").doubleClick();
+        $("span[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("span[data-test-id=date] input").setValue("24.02.2023");
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("+79181102009");
+        $("[class=checkbox__box]").click();
+        $("[class=button__text]").click();
+        $("[data-test-id=name] .input__sub").shouldBe(Condition.text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldNotValidPhone() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $("span[data-test-id=city] input").setValue ("Пермь");
+        $("span[data-test-id=date] input").doubleClick();
+        $("span[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("span[data-test-id=date] input").setValue("24.02.2023");
+        $("span[data-test-id=name] input").setValue("Ольга Бражкина");
+        $("span[data-test-id=phone] input").setValue("+7123456789");
+        $("[data-test-id=agreement]").click();
+        $x("//span[text()='Забронировать']").click();
+        $("[data-test-id=phone]").shouldBe(Condition.text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+
+    }
+
+
+    @Test
+    void shouldWithoutPhohe() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $("span[data-test-id=city] input").setValue ("Пермь");
+        $("span[data-test-id=date] input").doubleClick();
+        $("span[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("span[data-test-id=date] input").setValue("24.02.2023");
+        $("span[data-test-id=name] input").setValue("Ольга Бражкина");
+        $("span[data-test-id=phone] input").setValue("");
+        $("[data-test-id=agreement]").click();
+        $x("//span[text()='Забронировать']").click();
+        $x ("//span[contains (text (), 'Поле обязательно')]").shouldBe(visible);
+
+    }
+
+
+    @Test
+    void shouldNoCheckbox() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $("span[data-test-id=city] input").setValue ("Пермь");
+        $("span[data-test-id=date] input").doubleClick();
+        $("span[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("span[data-test-id=date] input").setValue("24.02.2023");
+        $("span[data-test-id=name] input").setValue("Ольга Бражкина");
+        $("span[data-test-id=phone] input").setValue("+71234567891");
+        $x("//span[text()='Забронировать']").click();
+        $x ("//span[text()='Я соглашаюсь с условиями обработки и использования моих персональных данных']").shouldBe(appear);
 
     }
 }
